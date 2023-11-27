4b155f6d9332763c39974c002026872764d92027
Code Mountains <thecodemountains@gmail.com>
2023-11-27 10:51:18 -0600

    typos

--- learn-terraform-docker-container/main.tf
+++ learn-terraform-docker-container/main.tf
@@ -17,7 +17,7 @@ resource "docker_image" "nginx" {
 }
 
 resource "docker_container" "nginx" {
-  image = docker_image.nginx.latest
+  image = docker_image.nginx
   name  = var.container_name
   ports {
     internal = 80
