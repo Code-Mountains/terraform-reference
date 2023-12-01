de4e4daa4f9c644f39f1eb0c91074fcdf803bea6
thecodemountains <thecodemountains@gmail.com>
2023-12-01 14:49:30 -0600

    install ansible

--- learn-terraform-aws-instance/main.tf
+++ learn-terraform-aws-instance/main.tf
@@ -18,6 +18,14 @@ resource "aws_instance" "app_server" {
   ami           = "ami-0aa7d40eeae50c9a9"
   instance_type = "t2.micro"
 
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo apt-get update
+              sudo apt-get install -y software-properties-common
+              sudo add-apt-repository --yes --update ppa:ansible/ansible
+              sudo apt-get install -y ansible
+              EOF
+
   tags = {
     Name = var.instance_name
   }
